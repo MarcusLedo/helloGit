84cf8116cb94044d2fe3ca9f3bc1744a153eb3bb
Marcus Ledo <sabaledo27@hotmail.com>
2021-01-11 01:04:45 -0300

    Acrescentada mensagem de boas vindas

--- src/application/Principal.java
+++ src/application/Principal.java
@@ -1,10 +1,21 @@
+//Marcus Ledo
+//Data: 11/01/2021
+//Objetivo: código exemplo para aprender o básico do git/GitHub
+
 package application;
+import java.util.Scanner;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		String name;
+		
+		System.out.print("Enter your name: ");
+		name = sc.nextLine();
+		
+		System.out.println("Welcome to GitHub! " + name);
+		sc.close();
 	}
 
 }
